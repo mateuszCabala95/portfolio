899452f372a88c14dd03e0d5c39c72dedbf1f964
Mateusz <mateusz.cabala95@gmail.com>
2020-08-18 17:14:42 +0200

    Add About section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Header = {
-    init: ()=>{
-      Header.toggleMenu()
+    init: () => {
+        Header.toggleMenu()
     },
 
     menuBtn: document.querySelector('.header__mobile-btn'),
@@ -8,12 +8,12 @@ const Header = {
     links: document.querySelectorAll('.nav__item'),
 
     toggleMenu: () => {
-        Header.menuBtn.addEventListener('click',()=>{
+        Header.menuBtn.addEventListener('click', () => {
             Header.menuBtn.classList.toggle('header__mobile-btn--open')
             Header.menu.classList.toggle('nav--open')
         })
 
-        Header.links.forEach(link => link.addEventListener('click', ()=>{
+        Header.links.forEach(link => link.addEventListener('click', () => {
             Header.menu.classList.toggle('nav--open');
             Header.menuBtn.classList.toggle('header__mobile-btn--open');
         }))
@@ -22,4 +22,40 @@ const Header = {
 
 }
 
-Header.init()
+
+const MoJSShape = {
+    init: () => {
+        window.addEventListener('click', (e) => {
+            MoJSShape.sparksShape.tune({
+                x: e.clientX,
+                y: e.clientY,
+            })
+                .replay()
+                .generate();
+        })
+    },
+
+    sparksShape: new mojs.Burst({
+        top: 0,
+        left: 0,
+        radius: {0: 30, easing: 'cubic.out'},
+        angle: {0: 90, easing: 'quad.out'},
+        count: 50,
+        children: {
+            shape: 'cross',
+            stroke: '#93A4AF',
+            points: 12,
+            radius: 10,
+            fill: 'none',
+            angle: {0: 360},
+            duration: 300
+        }
+    }),
+
+
+
+
+}
+
+Header.init();
+MoJSShape.init();
